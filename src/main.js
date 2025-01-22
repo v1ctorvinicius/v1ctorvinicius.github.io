d03566a0e6a75e9aa9d036cac4008f0faf572f20
v1ctorvinicius <victorviniciuspj@gmail.com>
2025-01-22 20:38:35 -0300

    feat: add vite

--- src/main.js
+++ src/main.js
@@ -1,24 +1,54 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import javascriptLogo from '/javascript.svg'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-setupCounter(document.querySelector('#counter'))
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+// Cube
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+// const material = new THREE.MeshBasicMaterial({ color: 0x00ffff });
+const material = new THREE.MeshStandardMaterial({ color: 0x00ffff });
+
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+// Lights
+const redLight = new THREE.PointLight(0xff0000);
+redLight.position.set(0, 1, 1);
+// redLight.position.set(2, 2, 2);
+
+scene.add(redLight);
+
+const blueLight = new THREE.PointLight(0x0000ff);
+blueLight.position.set(1, 0, 1);
+// blueLight.position.set(-2, -2, -2);
+scene.add(blueLight);
+
+const greenLight = new THREE.PointLight(0x00ff00);
+greenLight.position.set(0, 0, 1);
+scene.add(greenLight);
+
+// Camera positioning
+camera.position.z = 3;
+
+// Animation
+function animate() {
+    requestAnimationFrame(animate);
+
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+
+    // redLight.position.x = Math.sin(Date.now() * 0.001) * 2;
+    // redLight.position.y = Math.cos(Date.now() * 0.001) * 2;
+
+    // blueLight.position.x = Math.sin(Date.now() * 0.002) * 2;
+    // blueLight.position.y = Math.cos(Date.now() * 0.002) * 2;
+
+    renderer.render(scene, camera);
+}
+
+animate();
