7ea09cb778ce18ce066267f0132e27d83b2bd8e5
v1ctorvinicius <victorviniciuspj@gmail.com>
2025-02-09 20:21:46 -0300

    chore: adjust terrain shaders and sky

--- src/main.js
+++ src/main.js
@@ -5,21 +5,53 @@ import { resizeRendererToDisplaySize, loadShader } from "./util";
 import { createNoise2D } from "simplex-noise";
 import alea from "alea";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
-import vertexShader from "./shaders/vertexShader.glsl";
-import fragmentShader from "./shaders/fragmentShader.glsl";
+import vertexShader from "./shaders/waterVertexShader.glsl";
+import fragmentShader from "./shaders/waterFragmentShader.glsl";
 import { Sky } from "three/addons/objects/Sky.js";
 
 let scene, camera, renderer, controls, clock, renderTarget;
 let waterMesh, waterMaterial, depthMaterial;
+let terrainMaterial;
 let me;
+let shadowMatrix, shadowMapRenderTarget;
+let directionalLight, lightHelper;
+let dirLightShadowMap;
 
 const textureLoader = new THREE.TextureLoader();
 const textureFlare0 = textureLoader.load("/lensflare.jpg");
 const lensflare = new Lensflare();
 lensflare.addElement(new LensflareElement(textureFlare0, 512, 0));
 
-const prng = alea("v1ctorvinicius");
+const prng = alea("flamengo");
 const noise2D = createNoise2D(prng);
+
+async function main() {
+  clock = new THREE.Clock();
+  const canvas = document.querySelector("#canvas");
+  renderer = new THREE.WebGLRenderer({ antialias: true, canvas });
+  renderer.outputEncoding = THREE.sRGBEncoding;
+  renderer.shadowMap.enabled = true;
+  renderer.shadowMap.type = THREE.PCFShadowMap;
+
+  scene = new THREE.Scene();
+  camera = createCamera();
+  controls = createControls();
+
+  renderTarget = createDepthRenderTarget();
+  createSceneObjects();
+  requestAnimationFrame(render);
+}
+
+function render() {
+  const time = clock.getElapsedTime();
+  updateRendererSize();
+  captureSceneDepth();
+
+  updateWaterMaterialUniforms(time);
+
+  renderer.render(scene, camera);
+  requestAnimationFrame(render);
+}
 
 function generateTerrain(width, height, noise) {
   const geometry = new THREE.PlaneGeometry(width, height, 750, 750);
@@ -55,18 +87,6 @@ function generateTerrain(width, height, noise) {
   return geometry;
 }
 
-function render() {
-  const time = clock.getElapsedTime();
-  waterMaterial.uniforms.time.value = time;
-
-  updateRendererSize();
-  captureSceneDepth();
-  applyWaterEffects(time);
-
-  renderer.render(scene, camera);
-  requestAnimationFrame(render);
-}
-
 // Cria o render target para capturar a profundidade da cena
 function createDepthRenderTarget() {
   const target = new THREE.WebGLRenderTarget(
@@ -75,7 +95,7 @@ function createDepthRenderTarget() {
   );
   target.texture.minFilter = THREE.NearestFilter;
   target.texture.magFilter = THREE.NearestFilter;
-  target.texture.generateMipmaps = false;
+  // target.texture.generateMipmaps = true;
   target.depthTexture = new THREE.DepthTexture();
   target.depthTexture.type = THREE.UnsignedShortType;
   return target;
@@ -86,7 +106,7 @@ function createCamera() {
     75,
     window.innerWidth / window.innerHeight,
     0.01,
-    800
+    500
   );
   cam.position.set(-25, 10, -4);
   return cam;
@@ -100,29 +120,36 @@ function createControls() {
   return controls;
 }
 
-async function createSceneObjects() {
+function createSceneObjects() {
+  scene.fog = new THREE.Fog(new THREE.Color(0x9aabc3), 10, 400);
   // scene.background = new THREE.Color(0x9aabc3);
   const sky = new Sky();
+  sky.material.uniforms.turbidity.value = 1; // More haze
+  sky.material.uniforms.rayleigh.value = 0.1; // Blue scattering
+  sky.material.uniforms.mieCoefficient.value = 0.005; // Air particle density
+  sky.material.uniforms.mieDirectionalG.value = 0.8; // Sun glow intensity
+  renderer.toneMappingExposure = 0.5; // Adjust brightness
+
   sky.scale.setScalar(450000);
-  const phi = THREE.MathUtils.degToRad(45);
-  const theta = THREE.MathUtils.degToRad(180);
+  const phi = THREE.MathUtils.degToRad(65);
+  const theta = THREE.MathUtils.degToRad(0);
   const sunPosition = new THREE.Vector3().setFromSphericalCoords(1, phi, theta);
   sky.material.uniforms.sunPosition.value = sunPosition;
   scene.add(sky);
 
-  const directionalLight = new THREE.DirectionalLight(0xffffff, 5);
-  directionalLight.position.set(0, 200, -200);
-  directionalLight.castShadow = true;
-  directionalLight.shadow.mapSize.width = 2000;
-  directionalLight.shadow.mapSize.height = 2000;
+  directionalLight = new THREE.DirectionalLight(0xffffff, 5);
+  directionalLight.position.set(0, 10, 10);
   directionalLight.shadow.camera.near = 0.1;
   directionalLight.shadow.camera.far = 500;
-  directionalLight.shadow.bias = -0.002;
-  directionalLight.shadow.camera.top = 200;
-  directionalLight.shadow.camera.bottom = -200;
   directionalLight.shadow.camera.left = -200;
   directionalLight.shadow.camera.right = 200;
-  directionalLight.add(lensflare);
+  directionalLight.shadow.camera.top = 200;
+  directionalLight.shadow.camera.bottom = -200;
+  directionalLight.shadow.mapSize.width = 2000;
+  directionalLight.shadow.mapSize.height = 2000;
+  directionalLight.shadow.bias = -0.0002;
+  // directionalLight.add(lensflare);
+  directionalLight.castShadow = true;
 
   const directionalLightHelper = new THREE.DirectionalLightHelper(
     directionalLight
@@ -131,40 +158,121 @@ async function createSceneObjects() {
     new THREE.BoxGeometry(1, 1, 1),
     new THREE.MeshStandardMaterial({ color: 0xffffff })
   );
-  directionalLightTarget.position.set(0, 0, 0);
+  directionalLightTarget.position.set(0, 2, 0);
+  directionalLightTarget.castShadow = true;
   directionalLight.target = directionalLightTarget;
 
-  scene.add(new THREE.CameraHelper(directionalLight.shadow.camera));
+  lightHelper = new THREE.CameraHelper(directionalLight.shadow.camera);
+  scene.add(lightHelper);
   scene.add(directionalLightHelper);
   scene.add(directionalLightTarget);
   scene.add(directionalLight);
+
+  shadowMapRenderTarget = new THREE.WebGLRenderTarget(2048, 2048);
+  shadowMapRenderTarget.depthTexture = new THREE.DepthTexture();
+  shadowMapRenderTarget.depthTexture.type = THREE.FloatType;
+  shadowMapRenderTarget.depthTexture.format = THREE.DepthFormat;
+
+  dirLightShadowMap = directionalLight.shadow.map;
+
+  // console.log(
+  //   "GENERATE SCENE OBJECTS directionalLight.shadow.map ",
+  //   directionalLight.shadow.map
+  // );
+
+  // const shadowDebugMaterial = new THREE.MeshBasicMaterial({
+  //   map: directionalLight.shadow.map.texture,
+  // });
+  // const shadowDebugPlane = new THREE.Mesh(
+  //   new THREE.PlaneGeometry(5, 5),
+  //   shadowDebugMaterial
+  // );
+  // shadowDebugPlane.position.set(0, 10, 0); // Posicione acima do terreno
+  // scene.add(shadowDebugPlane);
 
   const terrainTexture = new THREE.TextureLoader().load("sand-texture.jpg");
   terrainTexture.wrapS = THREE.RepeatWrapping;
   terrainTexture.wrapT = THREE.RepeatWrapping;
   terrainTexture.repeat.set(100, 100);
   terrainTexture.rotation = Math.PI / 5;
-  const terrainGeometry = generateTerrain(500, 500, noise2D);
-  const terrainMaterial = new THREE.MeshStandardMaterial({
-    color: 0xccc5bf,
-    flatShading: false,
+
+  const terrainGeometry = generateTerrain(1000, 1000, noise2D);
+
+  terrainMaterial = new THREE.MeshStandardMaterial({
     map: terrainTexture,
+    fog: true,
+    // roughness: 10,
+    // metalness: 1,
   });
+  terrainMaterial.onBeforeCompile = function (shader) {
+    shader.uniforms.uTexture = { value: terrainTexture };
+    shader.uniforms.uMinHeight = { value: -0.3 };
+    shader.uniforms.uMaxHeight = { value: -0.5 };
+    shader.uniforms.uRepeat = { value: new THREE.Vector2(300, 300) };
+    shader.uniforms.uRotation = { value: Math.PI / 4 };
+    shader.vertexShader = shader.vertexShader.replace(
+      `#include <common>`,
+      `
+        #include <common>
+        varying vec2 vUv;
+        varying vec3 vPosition;
+        varying float vHeight;
+      `
+    );
+    shader.vertexShader = shader.vertexShader.replace(
+      `#include <uv_vertex>`,
+      `
+      #include <uv_vertex>
+      vUv = uv;
+      vPosition = position;
+      vHeight = position.z;
+    `
+    );
+    shader.fragmentShader = shader.fragmentShader.replace(
+      `#include <common>`,
+      `
+        #include <common>
+        uniform sampler2D uTexture;
+        uniform float uMinHeight;
+        uniform float uMaxHeight;
+        uniform vec2 uRepeat;
+        uniform float uRotation;
+        varying vec2 vUv;
+        varying float vHeight;
+      `
+    );
+    shader.fragmentShader = shader.fragmentShader.replace(
+      `#include <map_fragment>`,
+      `
+        #include <map_fragment>
+        vec2 repeatedUV = vUv * uRepeat;
+  
+        float cosR = cos(uRotation);
+        float sinR = sin(uRotation);
+        mat2 rotationMatrix = mat2(cosR, -sinR, sinR, cosR);
+  
+        vec2 rotatedUV = rotationMatrix * repeatedUV;
+        vec4 texColor = texture2D(uTexture, rotatedUV);
+  
+        float wetFactor = smoothstep(uMaxHeight, uMinHeight, vHeight);
+        vec3 darkenedColor = mix(texColor.rgb * (vec3(149, 127, 83) * 0.05) * 0.1, texColor.rgb, wetFactor);
+        diffuseColor.rgb = darkenedColor;
+      `
+    );
+  };
   const terrainMesh = new THREE.Mesh(terrainGeometry, terrainMaterial);
-  terrainMesh.receiveShadow = true;
   terrainMesh.castShadow = true;
+  terrainMesh.receiveShadow = true;
   terrainMesh.rotation.x = -Math.PI / 2;
   scene.add(terrainMesh);
 
-  // Material para captura de profundidade
-  depthMaterial = new THREE.MeshDepthMaterial({
-    depthPacking: THREE.RGBADepthPacking,
-    blending: THREE.NoBlending,
-  });
 
   waterMaterial = new THREE.ShaderMaterial({
     defines: { DEPTH_PACKING: 0, ORTHOGRAPHIC_CAMERA: 0 },
     uniforms: {
+      fogColor: { value: new THREE.Color(0x9aabc3) },
+      fogNear: { value: 0.0 },
+      fogFar: { value: 10.0 },
       time: { value: 0 },
       threshold: { value: 0.0 },
       foamScale: { value: 0.0 },
@@ -182,12 +290,14 @@ async function createSceneObjects() {
     vertexShader,
     fragmentShader,
     transparent: true,
-    fog: true,
     wireframe: false,
+    fog: true,
   });
 
   const waterGeometry = new THREE.PlaneGeometry(1000, 1000, 500, 500);
   waterMesh = new THREE.Mesh(waterGeometry, waterMaterial);
+  waterMesh.receiveShadow = true;
+  waterMesh.castShadow = true;
   waterMesh.rotation.x = -Math.PI * 0.5;
   waterMesh.position.y = -0.5;
   scene.add(waterMesh);
@@ -206,7 +316,7 @@ function updateRendererSize() {
 // Captura a profundidade da cena para os efeitos de água
 function captureSceneDepth() {
   waterMesh.visible = false;
-  scene.overrideMaterial = depthMaterial;
+  // scene.overrideMaterial = depthMaterial;
   renderer.setRenderTarget(renderTarget);
   renderer.render(scene, camera);
   renderer.setRenderTarget(null);
@@ -214,30 +324,10 @@ function captureSceneDepth() {
   waterMesh.visible = true;
 }
 
-function applyWaterEffects(time) {
+function updateWaterMaterialUniforms(time) {
   waterMaterial.uniforms.time.value = time;
   waterMaterial.uniforms.tDepth.value = renderTarget.depthTexture;
-}
-
-async function main() {
-  clock = new THREE.Clock();
-
-  const canvas = document.querySelector("#canvas");
-
-  renderer = new THREE.WebGLRenderer({ antialias: true, canvas });
-  renderer.outputEncoding = THREE.sRGBEncoding;
-  renderer.shadowMap.enabled = true;
-  renderer.shadowMap.type = THREE.PCFSoftShadowMap;
-
-  scene = new THREE.Scene();
-  camera = createCamera();
-
-  controls = createControls();
-  renderTarget = createDepthRenderTarget();
-
-  await createSceneObjects();
-
-  requestAnimationFrame(render);
+  waterMaterial.uniforms.tDepth.needsUpdate = true;
 }
 
 main();
